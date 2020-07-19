5d04c5ad5855a94297a599adf504cc54e3be6ad9
Your Name <you@example.com>
2020-07-20 01:53:08 +0800

    ok

--- go.mod
+++ go.mod
@@ -35,7 +35,7 @@ require (
 	github.com/goinggo/mapstructure v0.0.0-20140717182941-194205d9b4a9
 	github.com/golang/mock v1.3.1
 	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/google/go-cmp v0.4.0
+	github.com/google/go-cmp v0.5.0
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.6.2
 	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0
@@ -85,16 +85,15 @@ require (
 	go.mongodb.org/mongo-driver v1.2.0
 	go.uber.org/ratelimit v0.1.0
 	go.uber.org/zap v1.13.0 // indirect
-	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d
-	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
-	golang.org/x/sys v0.0.0-20200515095857-1151b9dac4a9 // indirect
-	golang.org/x/text v0.3.2
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	golang.org/x/text v0.3.3
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
 	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/grpc v1.27.0
-	google.golang.org/protobuf v1.24.0 // indirect
+	google.golang.org/grpc v1.26.0
 	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
 	gopkg.in/go-oauth2/redis.v3 v3.2.1
 	gopkg.in/go-playground/pool.v3 v3.1.1
--- sdk/etcd/etcd_test.go
+++ sdk/etcd/etcd_test.go
@@ -2,11 +2,11 @@ package test
 
 import (
 	"context"
-	"github.com/coreos/etcd/clientv3"
 	"go-demo/utils/env"
-	"runtime"
 	"testing"
 	"time"
+
+	"github.com/coreos/etcd/clientv3"
 )
 
 var (
@@ -19,14 +19,13 @@ func TestEtcd(t *testing.T) {
 	}
 	cli, err := clientv3.New(clientv3.Config{
 		// 集群列表
-		Endpoints:   []string{"ip:2379"},
+		Endpoints:   []string{"127.0.0.1:32769"},
 		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
 		t.Error(err)
 	}
 	defer cli.Close()
-
 	// 监听值
 	go func() {
 		watch := cli.Watch(ctx, "name")
@@ -35,58 +34,62 @@ func TestEtcd(t *testing.T) {
 	}()
 
 	// 存值
-	if resp, err := cli.Put(ctx, "name", "Hello", clientv3.WithPrevKV()); err != nil {
+	if resp, err := cli.Put(ctx, "/test", "Hello"); err != nil {
 		t.Error(err)
 	} else {
-		t.Log("旧值: ", string(resp.PrevKv.Value))
+		t.Log(resp)
 	}
 	// 取值
-	if resp, err := cli.Get(ctx, "name", clientv3.WithPrefix()); err != nil {
+	if resp, err := cli.Get(ctx, "/test"); err != nil {
 		t.Error(err)
 	} else {
-		t.Log("count: ", resp.Count)
-		t.Log("value: ", resp.Kvs)
+		t.Log(resp)
+		// t.Log("count: ", resp.Count)
+		// t.Log("value: ", resp.Kvs)
 	}
+	/*
 
-	// 改值
-	if resp, err := cli.Put(ctx, "name", "pibigstar", clientv3.WithPrevKV()); err != nil {
-		t.Error(err)
-	} else {
-		t.Log("旧值: ", string(resp.PrevKv.Value))
-	}
-	// 删值
-	if resp, err := cli.Delete(ctx, "name"); err != nil {
-		t.Error(err)
-	} else {
-		t.Log(resp.PrevKvs)
-	}
+		// 改值
+		if resp, err := cli.Put(ctx, "name", "pibigstar", clientv3.WithPrevKV()); err != nil {
+			t.Error(err)
+		} else {
+			t.Log("旧值: ", string(resp.PrevKv.Value))
+			t.Log("改值: ", resp)
+		}
+		// 删值
+		// if resp, err := cli.Delete(ctx, "name"); err != nil {
+		// 	t.Error(err)
+		// } else {
+		// 	t.Log(resp.PrevKvs)
+		// }
 
-	// 带租期的key
-	lease := clientv3.NewLease(cli)
-	// 申请一个5秒的租约(5s后key会被删除)
-	if response, err := lease.Grant(ctx, 5); err != nil {
-		t.Error(err)
-	} else {
+		// 带租期的key
+		lease := clientv3.NewLease(cli)
+		// 申请一个5秒的租约(5s后key会被删除)
+		if response, err := lease.Grant(ctx, 5); err != nil {
+			t.Error(err)
+		} else {
 
-		// 自动续约
-		if responses, err := lease.KeepAlive(ctx, response.ID); err == nil {
-			go func() {
-				for {
-					select {
-					case keepResp := <-responses:
-						if keepResp == nil {
-							t.Log("租约已失效或context已取消")
-							runtime.Goexit()
-						} else {
-							t.Log("自动续约...")
+			// 自动续约
+			if responses, err := lease.KeepAlive(ctx, response.ID); err == nil {
+				go func() {
+					for {
+						select {
+						case keepResp := <-responses:
+							if keepResp == nil {
+								t.Log("租约已失效或context已取消")
+								runtime.Goexit()
+							} else {
+								t.Log("自动续约...")
+							}
 						}
 					}
-				}
-			}()
-		}
+				}()
+			}
 
-		if _, err := cli.Put(ctx, "age", "18", clientv3.WithLease(response.ID)); err != nil {
-			t.Error(err)
+			if _, err := cli.Put(ctx, "age", "18", clientv3.WithLease(response.ID)); err != nil {
+				t.Error(err)
+			}
 		}
-	}
+	*/
 }
